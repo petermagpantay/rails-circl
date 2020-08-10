69938e45fdbb1fdc8f34d9558d30c83a5656f931
vincent wittek# <wittek.vincent@gmail.com>
2020-08-10 14:53:13 +0200

    added profiles controller, added validations to model

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,25 @@
+class ProfilesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show]
+  def show
+    @profile = User.find(params[:id])
+  end
+
+  def edit
+    @profile = User.find(params[:id])
+    authorize @profile
+  end
+
+  def update
+    @profile = User.find(params[:id])
+    @profile.update(profile_params)
+    authorize @profile
+
+    redirect_to profile_path(@profile)
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :age, :email, :password, :user_name, :bio)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,8 @@ class User < ApplicationRecord
   validates :age, inclusion: 18..99
   validates :user_name, presence: true
   validates :user_name, uniqueness: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :password, presence: true
 end
