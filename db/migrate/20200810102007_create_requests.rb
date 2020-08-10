146769a29332456ad30a7745fd60c23ed6985046
vincent wittek# <wittek.vincent@gmail.com>
2020-08-10 18:03:31 +0200

    added event request (no frontend yet)

--- db/migrate/20200810102007_create_requests.rb
+++ db/migrate/20200810102007_create_requests.rb
@@ -2,7 +2,7 @@ class CreateRequests < ActiveRecord::Migration[6.0]
   def change
     create_table :requests do |t|
 
-      t.string :status
+      t.string :status, default: "pending"
 
       t.timestamps
 
--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2020_08_10_103301) do
   end
 
   create_table "requests", force: :cascade do |t|
-    t.string "status"
+    t.string "status", default: "pending"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
