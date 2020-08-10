a536b85edf4c8087a00f712875484df5ef006cad
vincent wittek# <wittek.vincent@gmail.com>
2020-08-10 15:32:50 +0200

    fixed the problem with the migration

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :profile, only: [:show]
+  resources :profiles, only: [:show]
 
   resources :requests, only: [:index, :edit, :update]
 
--- db/migrate/20200810110946_add_columns_to_events.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToEvents < ActiveRecord::Migration[6.0]
-  def change
-    add_column :events, :event_date, :datetime
-    add_column :events, :event_time, :datetime
-  end
-end
