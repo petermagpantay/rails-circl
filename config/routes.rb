98ad82a5a3aeae7c2a39b1d083aff9852d7f05e0
Felipe Andres Lugo Salazar <felipeandreslugo@gmail.com>
2020-08-11 15:44:47 +0200

    seed.rb --> event title changed to 3 words

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: "pages#home"
 
   resources :profiles, only: [:show]
 
-  resources :requests, only: [:index, :edit, :update]
-
-    resources :events do
-      resources :requests, only: [:new, :create]
-      resources :comments, only: [:new, :create]
+  resources :requests, only: [:index, :show, :edit, :update] do
+    member do
+      patch :validate
     end
+  end
+
+  resources :events do
+    resources :requests, only: [:new, :create]
+    resources :comments, only: [:new, :create]
+  end
 end
