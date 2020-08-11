ca988e5b7a6fd3fa28e6a5d4d5ecf1bff1d192fc
vincent wittek <wittek.vincent@gmail.com>
2020-08-11 14:32:13 +0200

    editing profile is possible now

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   root to: 'pages#home'
 
-  resources :profiles, only: [:show]
+  resources :profiles, only: [:show, :edit, :update]
 
   resources :requests, only: [:index, :edit, :update]
 
