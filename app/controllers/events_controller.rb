506f9ad2366d939929d6ebad5af05394a141d401
Paul Wittchen <wittchenpaul@gmail.com>
2020-08-13 05:39:46 +0200

    Not working properly yet. Need some help from felipe and TA

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -33,32 +33,32 @@ class EventsController < ApplicationController
     # @eventShow = Event.find(params[:id])
     @event_show = Event.find(params[:id])
     @marker = 
-        [{
-          lat: @event_show.latitude,
-          lng: @event_show.longitude,
-          image_url: helpers.asset_url('marker.png')
-        }]
+      [{
+        lat: @event_show.latitude,
+        lng: @event_show.longitude,
+        image_url: helpers.asset_url('marker.png')
+      }]
   end
 
-    def new
-        @event = Event.new
-        authorize @event
-    end
+  def new
+    @event = Event.new
+    authorize @event
+  end
 
-    def create
-        @event = Event.new(event_params)
-        @event.user = current_user
-        authorize @event
-        if @event.save
-            redirect_to events_path
-        else
-            render :new
-        end
+  def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+    authorize @event
+    if @event.save
+      redirect_to events_path
+    else
+      render :new
     end
+  end
 
-    private
+  private
 
-    def event_params
-        params.require(:event).permit(:title, :description, :location, :event_date, :event_time)
-    end
+  def event_params
+    params.require(:event).permit(:title, :description, :location, :event_date, :event_time)
+  end
 end
