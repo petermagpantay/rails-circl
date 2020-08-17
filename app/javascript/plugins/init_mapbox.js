a407d345a0ee724bae15c75db02acb0564a53c0f
vincent wittek <wittek.vincent@gmail.com>
2020-08-17 13:21:09 +0200

    changed the color of the map to a more basic style which is more fitting

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -8,12 +8,12 @@ const initMapbox = () => {
       markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
       map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
     };
-  
+
     if (mapElement) { // only build a map if there's a div#map to inject into
       mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
       const map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/paul0711/ckdom0eaz25in1ip6rwcdvpvf'
+        style: 'mapbox://styles/mapbox/streets-v11',
       });
 
       const markers = JSON.parse(mapElement.dataset.markers);
