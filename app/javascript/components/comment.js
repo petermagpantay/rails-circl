3d3aba2ce07319e2fcf18d19aa5ede4027995868
Tarig Bouazzati <tbouzza@gmail.com>
2020-08-20 09:47:55 +0200

    basic structure for each page

--- app/javascript/components/comment.js
+++ app/javascript/components/comment.js
@@ -2,11 +2,14 @@ const text = document.getElementById('comment-event')
 const btn = document.getElementById('comment-btn')
 
 const initComment = () => {
+  if(btn){
     btn.addEventListener('click', () => {
-        if (text.value == "") {
-            alert("Please enter a valid comment")
-        }
+      if (text.value == "") {
+        alert("Please enter a valid comment")
+      }
     });
+
+  }
 }
 
 export { initComment }
